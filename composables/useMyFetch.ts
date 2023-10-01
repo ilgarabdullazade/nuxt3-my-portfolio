f7b7aaa0d476daedbf62b49a9cda28ddc58a443a
Ilgar <IlgarAbdullazade>
2023-10-01 14:46:35 +0400

    Fix locale problem

--- composables/useMyFetch.ts
+++ composables/useMyFetch.ts
@@ -4,12 +4,16 @@ import { defu } from 'defu'
 export function useMyFetch<T>(url: string, options: UseFetchOptions<T> = {}) {
   const { public: publicEnv } = useRuntimeConfig()
   const language = useCookie('language')
+  const { locale } = useI18n()
+  const langCode = ref<string>(
+    process.server ? locale.value : language.value ?? 'en'
+  )
 
   const defaults: UseFetchOptions<T> = {
     baseURL: publicEnv.apiBase,
     key: url,
     headers: {
-      'lang-code': language.value ?? 'en',
+      'lang-code': langCode.value,
     },
   }
 
