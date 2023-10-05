340738d93c6af6c66f3e355810711a3d848420bc
Ilgar <IlgarAbdullazade>
2023-10-05 21:14:43 +0400

    Add Hybrid Rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    '/**': { swr: 3600 },
+  },
   nitro: {
     compressPublicAssets: true,
   },
